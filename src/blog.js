fceb5fc0215e83f1a6a11b033b766d80ebddb6ce
Christopher Cailleaux <christopher.cailleaux@gmail.com>
2014-01-06 11:19:22 +0100

    blog def

---
+++ src/blog.js
@@ -0,0 +1,59 @@
+module.exports = {
+  /*
+  `createEntry(entry, callback)`
+  ----------------------------
+  */
+
+  createEntry: function(entry, callback) {
+  },
+
+  /*
+  `removeEntry(id, callback)`
+  ----------------------------
+
+  */
+
+  removeEntry: function(id, callback) {
+
+  },
+
+  /*
+  `editEntry(modifiedEntry, callback)`
+  ----------------------------
+
+  */
+
+  editEntry: function(modifiedEntry, callback) {
+
+  },
+
+    /*
+  `createCategory(name, callback)`
+  ----------------------------
+
+  */
+
+  createCategory: function(name, callback) {
+
+  },
+
+    /*
+  `removecategory(id, callback)`
+  ----------------------------
+
+  */
+
+  removecategory: function(id, callback) {
+
+  },
+
+    /*
+  `editCategory(newName, callback)`
+  ----------------------------
+
+  */
+
+  editCategory: function(newName, callback) {
+
+  }
+};
