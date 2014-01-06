2e0e3c4d7b39ab3f6f0295cc083b736fb64c2371
Christopher Cailleaux <christopher.cailleaux@gmail.com>
2014-01-06 21:29:49 +0100

    add db config fixed

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	var url = 'http://127.0.0.1:5984/',
-	var dbUsers = 'fluiddb_blog_entries',
-	var dbEntries = '',
+	var dbUsers = 'fluiddb_users',
+	var dbEntries = 'fluiddb_blog_entries',
 	var dbCategories ='fluiddb_blog_categories'
 }
