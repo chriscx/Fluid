cdb81fe81a89f6d0b12ed112b0023eb7b55aa30d
Christopher Cailleaux <christopher.cailleaux@gmail.com>
2014-01-07 04:22:05 +0100

    database conf

--- config/database.js
+++ config/database.js
@@ -1,7 +1,4 @@
 module.exports = {
 
-	host: function() { return 'http://127.0.0.1:5984/' },
-	dbUsers: function() { return 'fluiddb_users' },
-	dbEntries: function() { return 'fluiddb_blog_entries' },
-	dbCategories: function() { return 'fluiddb_blog_categories' }
+	host: function() { return 'http://127.0.0.1:5984/fluiddb_blog/' },
 }
