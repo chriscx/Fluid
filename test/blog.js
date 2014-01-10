068ff96bc35e9e173b1bde294e485484147bb9d2
Christopher Cailleaux <christopher.cailleaux@gmail.com>
2014-01-10 19:05:20 +0100

    unit test upgrade

--- test/blog.js
+++ test/blog.js
@@ -16,9 +16,39 @@ describe("blog", function() {
     mongoose.disconnect();
   });
 
-  it("save, get & delete an entry", function(next) {
+  it("create an entry", function(next) {
     var entry = { 
-      title: "This is a test entry",
+      title: "test1",
+      author: "root", 
+      url: "this-is-a-test-entry",
+      body: "This is me writing my first blog post",
+      tags: [{name: 'tag'}],
+      category: 'category',
+      comments: [{body: "this is a comment", date: new Date()}],
+      date: new Date(),
+      published: true
+    };
+    
+    return blog.createEntry(entry, function(err, data) {
+      if(err) {
+        console.log(err);
+        return next(err);
+      }
+      data.body.should.be.eql("This is me writing my first blog post");
+
+      blog.removeEntry(data._id, function(err, deletedEntry) {
+        if(err) {
+          console.log(err);
+          return next(err);
+        }
+        next();
+      });
+    });
+  });
+
+  it("get an entry", function(next) {
+    var entry = { 
+      title: "test2",
       author: "root", 
       url: "this-is-a-test-entry",
       body: "This is me writing my first blog post",
@@ -35,7 +65,7 @@ describe("blog", function() {
         return next(err);
       }
       // console.log('create: ' + data);
-      blog.getEntry("This is a test entry", function(err, retrievedEntry) {
+      blog.getEntry("test2", function(err, retrievedEntry) {
         if(err) {
           console.log(err);
           return next(err);
@@ -48,17 +78,45 @@ describe("blog", function() {
             console.log(err);
             return next(err);
           }
-          // console.log('remove: ' + deletedEntry);
-          blog.getEntry("This is a test entry", function(err, data) {
-            if(err) {
-              console.log(err);
-              return next(err);
-            }
-            // console.log("get 2: " + data);
-            data.should.be.empty;
-            next();
-          })
+          next();
         });
+      });
+    });
+  });
+
+  it("delete an entry", function(next) {
+    var entry = { 
+      title: "test3",
+      author: "root", 
+      url: "this-is-a-test-entry",
+      body: "This is me writing my first blog post",
+      tags: [{name: 'tag'}],
+      category: 'category',
+      comments: [{body: "this is a comment", date: new Date()}],
+      date: new Date(),
+      published: true
+    };
+    
+    return blog.createEntry(entry, function(err, data) {
+      if(err) {
+        console.log(err);
+        return next(err);
+      }
+      blog.removeEntry(data._id, function(err, deletedEntry) {
+        if(err) {
+          console.log(err);
+          return next(err);
+        }
+        // console.log('remove: ' + deletedEntry);
+        blog.getEntry("test3", function(err, data) {
+          if(err) {
+            console.log(err);
+            return next(err);
+          }
+          // console.log("get 2: " + data);
+          data.should.be.empty;
+          next();
+        })
       });
     });
   });
@@ -117,7 +175,7 @@ describe("blog", function() {
     });
   });
 
-  // it("create, get & save a category", function(next) {
+  // it("create, get & delete a category", function(next) {
 
   // });
 
