a89f9bd10adbf5b584cbb38b031d62e7bc94f880
Christopher Cailleaux <christopher.cailleaux@gmail.com>
2014-01-11 12:58:16 +0100

    Unit test renamed (fix)

---
+++ test/user.test.js
@@ -0,0 +1,95 @@
+var exec, should, user, mongoose, connexion;
+
+mongoose  = require('mongoose');
+mongoose.connect('mongodb://localhost/fluiddb');
+should = require('should');
+user = require('../src/user');
+
+describe("user", function() {
+
+  before(function() {
+    if(!mongoose.connection.readyState){
+      mongoose.connect('mongodb://localhost/fluiddb');
+    }
+  })
+
+  after(function() {
+    mongoose.disconnect();
+  });
+
+  it("create & delete a user", function(next) {
+    var newUser = { 
+      firstname: "firstname",
+      lastname: "lastname",
+      email: "firstname.lastname@fluid.org",
+      password: "password",
+      signupDate: new Date(),
+    };
+
+    return user.create(newUser, function(err, createdUser) {
+      if(err) {
+        return next(err);
+      }
+      user.get(createdUser._id, function(err, retrievedUser) {
+        if(err) {
+          return next(err);
+        }
+        retrievedUser[0].email.should.eql("firstname.lastname@fluid.org");
+        var id = retrievedUser[0]._id;
+        user.remove(id, function(err, deletedUser) {
+          if(err) {
+            return next(err);
+          }
+          user.get(id, function(err, data) {
+            if(err){
+              return next(err);
+            }
+            data.should.be.empty;
+            next();
+          })
+        })
+      });
+    });
+  });
+
+  it("checks the password", function(next) {
+    var newUser = { 
+      firstname: "firstname",
+      lastname: "lastname",
+      email: "firstname.lastname@fluid.org",
+      password: "password",
+      signupDate: new Date(),
+    };
+
+    return user.create(newUser, function(err, createdUser) {
+      if(err) {
+        return next(err);
+      }
+      user.checkPassword("firstname.lastname@fluid.org", "password", function(err, res) {
+        if(err) {
+          return next(err);
+        }
+        res.check.should.be.ok;
+        user.get(createdUser._id, function(err, retrievedUser) {
+          if(err) {
+            return next(err);
+          }
+          retrievedUser[0].email.should.eql("firstname.lastname@fluid.org");
+          var id = retrievedUser[0]._id;
+          user.remove(id, function(err, deletedUser) {
+            if(err) {
+              return next(err);
+            }
+            user.get(id, function(err, data) {
+              if(err){
+                return next(err);
+              }
+              data.should.be.empty;
+              next();
+            })
+          })
+        });
+      });
+    });
+  });
+});
